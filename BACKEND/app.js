575efd08db812d362e51b33372b73d7fa25f5c0f
Naveesha Jayah <naveeshabjayah@gmail.com>
2025-02-27 22:08:35 +0530

    Main Environment
    Main Environment for backend and frontend

---
+++ BACKEND/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require ("mongoose");
+
+const dotenv = require("dotenv");
+require("dotenv").config();
+
+
+
+const app = express();
+
+//middleware
+app.use("/",(req,res,next)=>{
+    res.send("It is working");
+})
+
+//database connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB ✅");
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT} 🚀`);
+    });
+})
+.catch((err) => console.log("MongoDB Connection Error ❌:", err));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import NavBar from './Others/NavBar'
+import Footer from './Others/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <Routes>
+         
+        </Routes>
+      </main>
+      <Footer />
+
+      
+    </div>
+  )
+}
+
+export default App
