90cddaeddbf7b5689d3c38bd92903737e51f175e
Naveesha Jayah <naveeshabjayah@gmail.com>
2025-03-11 20:26:01 +0530

    TimeTable
    TimeTableBackend

---
+++ BACKEND/Route/timeTableRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const timeTableController = require("../Controllers/timeTableController");
+const timeTableModel = require("../Model/timeTableModel");
+
+router.get("/", timeTableController.getTimeTable);
+router.post("/", timeTableController.addTimeTable);
+router.get("/:id", timeTableController.getTimeTableById);
+router.put("/:id", timeTableController.updateTimeTable);
+router.delete("/:id", timeTableController.deleteTimeTable);
+
+module.exports = router;
--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require ("mongoose");
-
 const dotenv = require("dotenv");
+const cors = require("cors");
 require("dotenv").config();
+
+
+const timeTableRoute = require("./Route/timeTableRoute");
 
 
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 //middleware
-app.use("/",(req,res,next)=>{
-    res.send("It is working");
-})
+
+app.use("/timeTable", timeTableRoute);
 
 //database connection
 mongoose.connect(process.env.MONGO_URI)
