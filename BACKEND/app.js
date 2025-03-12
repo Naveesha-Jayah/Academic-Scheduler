b76a67561a399d8d3f5e4b99158769aa9685d3ae
Naveesha Jayah <naveeshabjayah@gmail.com>
2025-03-12 13:54:55 +0530

    Update NavBar & Footer
    Update NavBar & Footer
    12/3

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 require("dotenv").config();
 
-
 const timeTableRoute = require("./Route/timeTableRoute");
-
-
 
 const app = express();
 
@@ -19,11 +16,12 @@ app.use(express.json());
 app.use("/timeTable", timeTableRoute);
 
 //database connection
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("Connected to MongoDB ✅");
     app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT} 🚀`);
+      console.log(`Server running on port ${process.env.PORT} 🚀`);
     });
-})
-.catch((err) => console.log("MongoDB Connection Error ❌:", err));
+  })
+  .catch((err) => console.log("MongoDB Connection Error ❌:", err));
