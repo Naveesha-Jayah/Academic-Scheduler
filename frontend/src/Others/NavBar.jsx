1677b21eaeb10896a74e0f6423487bca7a6e52ab
Naveesha Jayah <179082568+YashiraPerera@users.noreply.github.com>
2025-03-20 13:49:50 +0530

    Done Changes 20/3
    Room & Resources

--- frontend/src/Others/NavBar.jsx
+++ frontend/src/Others/NavBar.jsx
@@ -98,12 +98,21 @@ const NavBar = () => {
             </li>
             <li>
               <Link
-                to="/schedule"
+                to="/Room"
                 className="flex items-center p-3 rounded-lg hover:bg-base-200 transition"
               >
-                🚪 Rooms & Resources
+                🚪 Rooms
               </Link>
             </li>
+            <li>
+              <Link
+                to="/Resource"
+                className="flex items-center p-3 rounded-lg hover:bg-base-200 transition"
+              >
+                💻 Resources
+              </Link>
+            </li>
+            <li></li>
             <li>
               <Link
                 to="/schedule"
@@ -117,7 +126,7 @@ const NavBar = () => {
 
         {/* Overlay (Click to Close) */}
         <div
-          className="flex-1 bg-white bg-opacity-50"
+          className="flex-1 backdrop:brightness-200 bg-opacity-40" // Changed to a more transparent overlay
           onClick={() => setIsOpen(false)}
         ></div>
       </div>
